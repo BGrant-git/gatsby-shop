44b6322a8378686fb89d618a528f16bf257c111f
bengrant <bengranthome@gmail.com>
2020-11-26 19:31:10 +0000

    before merge

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,29 @@ module.exports = {
         accessToken: "5225afa1afaeae4e57839085b46f6a33",
         apiVersion: "2020-10",
         includeCollections: ["shop"],        
+        shopifyQueries: {
+          products:`
+          query MyQuery {
+            allShopifyCollection {
+              nodes {
+                title
+                products {
+                  title
+                  tags
+                  images {
+                    originalSrc
+                  }
+                  description
+                  variants {
+                    price
+                  }
+                }
+              }
+            }
+          }
+          
+          `
+        }
       },
     },
   ]
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,26 @@
 import React from "react"
+import Client from 'shopify-buy'
 
-export default function Home() {
+const Home = () => {
+
+  const user = require('../../gatsby-config')
+  console.log({user})
+
+  // const client = Client.buildClient({
+  //   domain: 'shop1-habibi.myshopify.com',
+  //   storefrontAccessToken: '5225afa1afaeae4e57839085b46f6a33'
+  // });
+  // // fetch all products 
+  //   client.product.fetchAll().then((products) => {
+  //     console.log(products);
+  //   });
+  //   client.collection.fetchAllWithProducts().then((collections) => {
+  //     console.log(collections);
+  //     console.log(collections[0].products[0].title);
+  //   });
+
   return <div>Hello world!</div>
 }
+
+export default Home
+
